ed67c740313031b606b2776a3b62826190d0821c
vickygioviBYU <can23001.byui.edu>
2023-09-23 10:31:07 -0300

    CSS Combinators

---
+++ JSON Presentation/index.js
@@ -0,0 +1,11 @@
+// JSON encoded
+
+let json = JSON.parse('{"employee":{"name":"sonoo","salary":56000,"married":true}}');
+
+// console.log(json.employee.name);
+
+// JSON as a string
+
+let string = JSON.stringify(json);
+
+console.log(string);
