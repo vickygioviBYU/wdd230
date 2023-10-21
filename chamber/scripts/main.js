e31a85c17b3293a8085f3cdd6813d2dcf6031147
vickygioviBYU <can23001.byui.edu>
2023-10-21 18:11:20 -0300

    Chamber W07

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -12,4 +12,29 @@ function toggleDarkMode() {
     element.classList.toggle("dark-mode");
     footer.classList.toggle("dark-mode-footer");
     h2.classList.toggle("dark-mode-h2")
-  }
+}
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    let datePlusDay = Number(localStorage.getItem("date")) + 86400000
+    if (Date.now() < datePlusDay) {
+        alert("Back so soon! Awesome!")
+    } else {
+        let days = Math.floor((Date.now() - Number(localStorage.getItem("date"))) / 86400000)
+        if (days !== 1) {
+            alert("You last visited" + days + "days ago.")
+        } else {}
+            alert("You last visited" + days + "day ago.")
+    }
+} else {
+	alert("Welcome! Let us know if you have any questions.");
+}
+
+// 4️⃣ increment the number of visits by one.
+numVisits++;
+
+// 5️⃣ store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numVisits-ls", numVisits);
+
+localStorage.setItem("date", Date.now())
