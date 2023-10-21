3d6e135849dd4d86d5db45b78562427f5100f6f5
vickygioviBYU <can23001.byui.edu>
2023-10-21 13:06:52 -0300

    Dark Mode

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,2 +1,15 @@
 let oLastModif = new Date(document.lastModified);
 document.querySelector("#lastMod").innerHTML = oLastModif;
+
+document.querySelector("#toggleDarkMode").addEventListener("click", () => {
+    toggleDarkMode()
+})
+
+function toggleDarkMode() {
+    var element = document.body;
+    var footer = document.querySelector("footer")
+    var h2 = document.querySelector(".h2")
+    element.classList.toggle("dark-mode");
+    footer.classList.toggle("dark-mode-footer");
+    h2.classList.toggle("dark-mode-h2")
+  }
