ac6900a11a7106ac923210dba5dace565f7c68a5
vickygioviBYU <vickygioviBYU>
2023-11-24 13:50:29 -0300

    Weather image fixed

--- scripts/weather.js
+++ scripts/weather.js
@@ -9,43 +9,48 @@ async function fetchWeather() {
     const cond = document.querySelector("#conditions")
     cond.textContent = conditions
     const img = document.querySelector("#image")
-    if (conditions == "clear sky") {
-        const image = document.createElement("img")
-        image.setAttribute("src", "images/clear sky.png")
-        img.appendChild(image)
-    } else if (conditions == "few clouds") {
-        const image = document.createElement("img")
-        image.setAttribute("src", "images/few clouds.png")
-        img.appendChild(image)
-    } else if (conditions == "scattered clouds") {
-        const image = document.createElement("img")
-        image.setAttribute("src", "images/scattered cloud.png")
-        img.appendChild(image)
-    } else if (conditions == "broken clouds") {
-        const image = document.createElement("img")
-        image.setAttribute("src", "images/broken clouds.png")
-        img.appendChild(image)
-    } else if (conditions == "shower rain") {
-        const image = document.createElement("img")
-        image.setAttribute("src", "images/shower rain.png")
-        img.appendChild(image)
-    } else if (conditions == "rain") {
-        const image = document.createElement("img")
-        image.setAttribute("src", "images/rain.png")
-        img.appendChild(image)
-    } else if (conditions == "thunderstorm") {
-        const image = document.createElement("img")
-        image.setAttribute("src", "images/thunderstorm.png")
-        img.appendChild(image)
-    } else if (conditions == "snow") {
-        const image = document.createElement("img")
-        image.setAttribute("src", "images/snow.png")
-        img.appendChild(image)
-    } else if (conditions == "mist") {
-        const image = document.createElement("img")
-        image.setAttribute("src", "images/mist.png")
-        img.appendChild(image)
-    }
+    let weatherSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+
+    const image = document.createElement("img")
+    image.setAttribute("src", weatherSrc)
+    img.appendChild(image)
+    // if (conditions == "clear sky") {
+    //     const image = document.createElement("img")
+    //     image.setAttribute("src", "images/clear sky.png")
+    //     img.appendChild(image)
+    // } else if (conditions == "few clouds") {
+    //     const image = document.createElement("img")
+    //     image.setAttribute("src", "images/few clouds.png")
+    //     img.appendChild(image)
+    // } else if (conditions == "scattered clouds") {
+    //     const image = document.createElement("img")
+    //     image.setAttribute("src", "images/scattered cloud.png")
+    //     img.appendChild(image)
+    // } else if (conditions == "broken clouds") {
+    //     const image = document.createElement("img")
+    //     image.setAttribute("src", "images/broken clouds.png")
+    //     img.appendChild(image)
+    // } else if (conditions == "shower rain") {
+    //     const image = document.createElement("img")
+    //     image.setAttribute("src", "images/shower rain.png")
+    //     img.appendChild(image)
+    // } else if (conditions == "rain") {
+    //     const image = document.createElement("img")
+    //     image.setAttribute("src", "images/rain.png")
+    //     img.appendChild(image)
+    // } else if (conditions == "thunderstorm") {
+    //     const image = document.createElement("img")
+    //     image.setAttribute("src", "images/thunderstorm.png")
+    //     img.appendChild(image)
+    // } else if (conditions == "snow") {
+    //     const image = document.createElement("img")
+    //     image.setAttribute("src", "images/snow.png")
+    //     img.appendChild(image)
+    // } else if (conditions == "mist") {
+    //     const image = document.createElement("img")
+    //     image.setAttribute("src", "images/mist.png")
+    //     img.appendChild(image)
+    // }
 }
 
 fetchWeather()
